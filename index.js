e8219a02f14d42983b2dc26713e7b546a867fa2d
parpiyev111 <parpiyev111@gmail.com>
2021-11-25 18:41:02 +0500

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const app = require('express')();
 const axios = require("axios")
+const iplocate = require('node-iplocate');
 
 app.get('/', async (req, res) => {
 
-    // const iplocate = require('node-iplocate');
 
-    // const data = await iplocate(req.headers['x-forwarded-for'] || req.socket.remoteAddress);
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
-    const apiKey = 'abb0cae033b33812aa6153dedabb57017e1ee677ff23aa30f48e74a5';
-    const { data } = await axios.get(`https://api.ipdata.co/${ip}?api-key=${apiKey}`)
+    const data = await iplocate(ip);
     return res.json({ data })
 })
 
