999d28813ce070aac0a1a49347ed7720990e5eaf
parpiyev111 <parpiyev111@gmail.com>
2021-11-25 17:13:32 +0500

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const app = require('express')();
 const axios = require("axios")
 
-// source file is iso-8859-15 but it is converted to utf-8 automatically
-
 app.get('/', async (req, res) => {
+
+    // const iplocate = require('node-iplocate');
+
+    // const data = await iplocate(req.headers['x-forwarded-for'] || req.socket.remoteAddress);
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
     const apiKey = 'abb0cae033b33812aa6153dedabb57017e1ee677ff23aa30f48e74a5';
-    const { data } = await axios.get(`https://api.ipdata.co?api-key=${apiKey}`)
+    const { data } = await axios.get(`https://api.ipdata.co/${ip}?api-key=${apiKey}`)
     return res.json({ data })
 })
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`http://localhost:${port}/`);
-})
+});
